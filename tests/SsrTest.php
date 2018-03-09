de810269243ffde9a4a64ca854209026fee3258c
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-03-09 12:53:26 +0100

    Fix entry in tests

--- tests/SsrTest.php
+++ tests/SsrTest.php
@@ -7,7 +7,7 @@ class SsrTest extends TestCase
     /** @test */
     public function it_can_render_a_javascript_app()
     {
-        $result = ssr('js/app.js')->enabled()->debug()->render();
+        $result = ssr('js/app-server.js')->enabled()->debug()->render();
 
         $this->assertEquals('<p>Hello, world!</p>', $result);
     }
